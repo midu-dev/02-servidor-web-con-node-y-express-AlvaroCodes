2a995b2b5ce0fad100a6528b1df83718c1539b31
AlvaroCodes <aramirezm098@gmail.com>
2023-08-04 20:17:46 +0200

    Se empieza express.js

--- express.js
+++ express.js
@@ -1,7 +1,58 @@
+const Express = require('express')
+const Path = require('node:path')
+
+const PORT = 1234
+// const PATH_LOGO = Path.join(__dirname, 'assets', 'logo.webp')
+const PATH_LOGO = Path.join(__dirname, 'assets')
+
 // Ejercicio 2: crear servidor HTTP con Express
 function startServer () {
-  // const server = app.listen(...)
-  // return server
+  const app = Express()
+  app.disable('x-powered-by')
+
+  // Middleware
+  app.use(Express.json())
+  app.use(Express.static(PATH_LOGO))
+
+  // Routers
+  routers(app)
+  // Server
+  const server = app.listen(PORT, () => {
+    console.log('Server in ' + PORT)
+  })
+  return server
+}
+
+function routers (app) {
+  app.all('/', ({ method }, res) => {
+    if (method === 'GET') {
+      res.send('<h1>¡Hola mundo!</h1>')
+    } else {
+      res.status(405).send('<h1>405</h1>')
+    }
+  })
+
+  // app.get('/logo.webp', (req, res) => {
+  //   res.contentType('image/webp')
+  //   res.sendFile(PATH_LOGO)
+  // })
+
+  app.all('/contacto', ({ body, method }, res) => {
+    console.log(method)
+    if (method === 'POST') {
+      res.status(201).send(body)
+    } else {
+      res.status(405).send('<h1>405</h1>')
+    }
+  })
+
+  app.all('/404', (req, res) => {
+    res.status(404).send('<h1>404</h1>')
+  })
+
+  app.all('*', (req, res) => {
+    res.status(404).send('<h1>404</h1>')
+  })
 }
 
 module.exports = {
